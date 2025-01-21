d4d537cab47b3406b75ec97f700a95a6bbfd815c
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2025-01-21 21:46:25 +0700

    database i think 2

--- src/main/java/com/dentalclinic/entities/Patient.java
+++ src/main/java/com/dentalclinic/entities/Patient.java
@@ -19,4 +19,21 @@ public class Patient {
     
     @Column(name = "last_name")
     private String lastName;
+
+    public long getId() {
+        return id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
 }
