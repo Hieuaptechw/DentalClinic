c39149116a59aff485df5466abdb9425bc26a6f8
HieuTrann18 <hieu.tt.2455@aptechlearning.edu.vn>
2025-02-13 04:32:32 +0700

    update
    update

--- src/main/java/com/dentalclinic/views/pages/administration/SalaryPage.java
+++ src/main/java/com/dentalclinic/views/pages/administration/SalaryPage.java
@@ -1,8 +1,21 @@
 package com.dentalclinic.views.pages.administration;
 
+import com.dentalclinic.entities.Salary;
 import com.dentalclinic.views.pages.AbstractPage;
 import com.dentalclinic.views.pages.Page;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 
 @Page(name="Lương", icon="images/revenue.png", fxml="administration/salary.fxml")
 public class SalaryPage extends AbstractPage {
+    @FXML
+    private TableView<Salary> salaryTable;
+
+    @FXML
+    private TableColumn<Salary, String> nameColumn;
+
+
+
+
 }
